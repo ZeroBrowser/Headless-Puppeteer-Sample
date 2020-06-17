cb0b3b9e229000bdb1e98f9e21d165531e312db9
Raha Mohebbi <rahamohebbi@gmail.com>
2020-06-16 19:27:33 -0700

    cleanup urls

--- index.ts
+++ index.ts
@@ -7,36 +7,25 @@ import vision = require('@google-cloud/vision');
 
 class Startup {
 
-    urls: Array<string>;
+    blogUrls: Array<string>;
+    corporateUrls: Array<string>;
     helper: Helper;
+
     constructor() {
         this.helper = new Helper();
 
-        // this.urls = new Array<any>(
-        //     { 'name': "ford-blog", 'url': "https://media.ford.com/content/fordmedia/fna/us/en/news.html" },        
-        //     { 'name': "chevron-blog", 'url': "https://www.chevron.com/stories" },            
-        //     { 'name': "amazon-blog", 'url': "https://aws.amazon.com/blogs" },            
-        //     { 'name': "google-blog", 'url': "https://www.blog.google" },            
-        //     { 'name': "chase-blog", 'url': "https://www.chase.com/news" },
-        //     { 'name': "coca-cola-blog", 'url': "https://www.coca-colacompany.com/news" },            
-        //     { 'name': "tesla-blog", 'url': "https://www.tesla.com/blog" },            
-        //     { 'name': "microsoft-blog", 'url': "https://techcommunity.microsoft.com/t5/custom/page/page-id/Blogs" });
+        this.blogUrls = new Array<any>(
+            { 'name': "ford-blog", 'url': "https://media.ford.com/content/fordmedia/fna/us/en/news.html" },
+            { 'name': "chevron-blog", 'url': "https://www.chevron.com/stories" },
+            { 'name': "amazon-blog", 'url': "https://aws.amazon.com/blogs" },
+            { 'name': "tesla-blog", 'url': "https://www.tesla.com/blog" },
+            { 'name': "microsoft-blog", 'url': "https://techcommunity.microsoft.com/t5/custom/page/page-id/Blogs" }
+        );
 
-        // this.urls = new Array<any>(
-        //     { 'name': "ford-blog", 'url': "https://media.ford.com/content/fordmedia/fna/us/en/news.html" }, //W
-        //     { 'name': "chevron-blog", 'url': "https://www.chevron.com/stories" },         //W
-        //     { 'name': "amazon-blog", 'url': "https://aws.amazon.com/blogs" },             //W
-        //     { 'name': "google-blog", 'url': "https://www.blog.google" },                  //W
-        //     { 'name': "chase-blog", 'url': "https://www.chase.com/news" },                //W
-        //     { 'name': "coca-cola-blog", 'url': "https://www.coca-colacompany.com/news" }, //W
-        //     { 'name': "tesla-blog", 'url': "https://www.tesla.com/blog" },                //W
-        //     { 'name': "microsoft-blog", 'url': "https://techcommunity.microsoft.com/t5/custom/page/page-id/Blogs" }       //W
-        // );
-
-        this.urls = new Array<any>(
+        this.corporateUrls = new Array<any>(
             { 'name': "tesla-corp", 'url': "https://www.tesla.com" },
             { 'name': "microsoft-corp", 'url': "https://www.microsoft.com/en-us" },
-            //{ 'name': "amazon-corp", 'url': "https://www.amazon.com" },
+            //{ 'name': "amazon-corp", 'url': "https://www.amazon.com" },               //some issues with amazon
             { 'name': "chevron-corp", 'url': "https://www.chevron.com" },
             { 'name': "ford-corp", 'url': "https://www.ford.com" }
         );
@@ -52,7 +41,7 @@ class Startup {
 
         let results = new Array<any>();
 
-        await this.start(0, this.urls, results);
+        await this.start(0, this.corporateUrls, results);
 
         this.save(results, "results.json");
 
@@ -76,7 +65,7 @@ class Startup {
 
         try {
             console.log(`current website: ${url['url']}`);
-            
+
             let page = await this.helper.goto(url['url']);
 
             await this.helper.delay(5000);
@@ -84,13 +73,9 @@ class Startup {
             await page.screenshot({ path: imageName, fullPage: true });
             await page.close();
             text = await this.convertImageToTextWithGoogleVision(imageName);
-            //let text = await this.convertImageToText(imageName); //alternative to Google Vision API. It is much slower currently!
-
-        } catch (error) {            
+        } catch (error) {
             console.log(error);
         }
-
-        
 
         let lowerCaseText = text && text.length > 0 ? text.toLowerCase() : "";
         let totalCovidPhrasesFound = this.textSearch(lowerCaseText, new Array<string>("corona", "coronavirus", "covid-19", "pandemic", "outbreak", "respirators", "masks"));
@@ -131,45 +116,6 @@ class Startup {
             });
 
             resolve(finalText);
-        });
-    }
-
-    async convertImageToText(imageName: string): Promise<string> {
-        const worker1 = createWorker({
-            logger: m => console.log(m), // Add logger here
-        });
-        const worker2 = createWorker({
-            logger: m => console.log(m), // Add logger here
-        });
-
-        const scheduler = createScheduler();
-
-        const rectangle = { left: 0, top: 100, width: 800, height: 600 };
-
-        return new Promise((resolve, reject) => {
-            (async () => {
-                await worker1.load();
-                await worker1.loadLanguage('eng');
-                await worker1.initialize('eng');
-
-                await worker2.load();
-                await worker2.loadLanguage('eng');
-                await worker2.initialize('eng');
-
-                scheduler.addWorker(worker1);
-                scheduler.addWorker(worker2);
-
-                //const { data: { text, box } } = await worker.recognize(imageName);
-
-                const results = await Promise.all(Array(10).fill(0).map(() => (
-                    scheduler.addJob('recognize', imageName)
-                )));
-
-                //await worker.terminate();
-                await scheduler.terminate();
-                console.log(imageName + ":" + results);
-                resolve("");
-            })();
         });
     }
 
